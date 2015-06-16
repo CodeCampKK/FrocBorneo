eaae2dac67b0e7800d4b76d5960ef2ea9d132a4b
John Wong <john@flexnode.com>
2015-06-16 22:41:56 +0800

    User creation and listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
   end
 
   def show
@@ -9,7 +13,12 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def update
